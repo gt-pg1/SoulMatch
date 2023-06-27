cdc94f9902b164333058d9e381b956bfae74f4f2
gt-pg1 <shot66766@gmail.com>
2023-06-27 19:40:42 +0400

    Added test for UserSerializer

--- SoulMatcher/soulmate/tests.py
+++ SoulMatcher/soulmate/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .serializers import UserSerializer
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserSerializerTest(TestCase):
+    def test_user_serializer(self):
+        user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
+        serializer = UserSerializer(user)
+
+        self.assertEqual(set(serializer.data.keys()), {'id', 'username', 'email'})
+
+        self.assertEqual(serializer.data['username'], user.username)
+        self.assertEqual(serializer.data['email'], user.email)
