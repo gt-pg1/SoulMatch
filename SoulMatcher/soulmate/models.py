fe6a258775e10bb409141a1328d2e89160a02f41
gt-pg1 <shot66766@gmail.com>
2023-06-27 18:12:42 +0400

    Created Priority model

--- SoulMatcher/soulmate/models.py
+++ SoulMatcher/soulmate/models.py
@@ -8,3 +8,18 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class Priority(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    aspect = models.CharField(max_length=100)
+    attitude = models.CharField(
+        max_length=10,
+        choices=[
+            ('positive', 'Положительное'),
+            ('negative', 'Отрицательное'),
+        ]
+    )
+    weight = models.PositiveIntegerField(choices=[(i, str(i)) for i in range(1, 11)])
+
+    def __str__(self):
+        return f"{self.aspect} ({self.attitude}, {self.weight})"
