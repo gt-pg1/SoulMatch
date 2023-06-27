63449a42aca3b87d43d4ed3be3bbf6e511399c3a
gt-pg1 <shot66766@gmail.com>
2023-06-27 17:54:43 +0400

    Created CustomUser model for custom authentifications and in define like AUTH_USER_MODEL in settings.py

--- SoulMatcher/soulmate/models.py
+++ SoulMatcher/soulmate/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email_confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
