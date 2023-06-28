f039d9d21ceaadbc7e3e20633479b107056aeb7f
gt-pg1 <shot66766@gmail.com>
2023-06-28 12:46:32 +0400

    Added route for email confirmations

--- SoulMatcher/soulmate/urls.py
+++ SoulMatcher/soulmate/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register
+from . import views
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
+    path('email-confirmation/<str:token>/', views.email_confirmation, name='email-confirmation'),
 ]
