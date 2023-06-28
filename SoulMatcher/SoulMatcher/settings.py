c576da657e7aeaa0958acfd094df4f61141fc8ec
gt-pg1 <shot66766@gmail.com>
2023-06-28 13:14:57 +0400

    Configured authentication with JSON Web Tokens

--- SoulMatcher/SoulMatcher/settings.py
+++ SoulMatcher/SoulMatcher/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'soulmate'
 
 ]
@@ -127,3 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
--- SoulMatcher/soulmate/urls.py
+++ SoulMatcher/soulmate/urls.py
@@ -1,7 +1,12 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('email-confirmation/<str:token>/', views.email_confirmation, name='email-confirmation'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
