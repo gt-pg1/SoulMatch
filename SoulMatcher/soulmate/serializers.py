c1c77917f9349844965794c2218084458e4463fb
gt-pg1 <shot66766@gmail.com>
2023-06-28 09:57:08 +0400

    Redefined a create method in UserSerialazer for password hashing

--- SoulMatcher/soulmate/serializers.py
+++ SoulMatcher/soulmate/serializers.py
@@ -9,3 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Метод create_user обеспечивает хэширование пароля
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
