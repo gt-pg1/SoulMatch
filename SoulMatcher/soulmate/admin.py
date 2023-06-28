90022e5c086d326c5a3f4462001553b5e5bdcec0
gt-pg1 <shot66766@gmail.com>
2023-06-28 20:48:31 +0400

    Added admin panel

--- SoulMatcher/soulmate/admin.py
+++ SoulMatcher/soulmate/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import CustomUser, Priority
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'email_confirmed']
+    list_filter = ['email_confirmed']
+    search_fields = ['username', 'email']
+    # Дополнительные настройки админки для CustomUser
+
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ['aspect', 'attitude', 'weight', 'user']
+    list_filter = ['aspect', 'attitude']
+    search_fields = ['aspect', 'attitude', 'user__username']
+    # Дополнительные настройки админки для Priority
