1acee2f9f98f5dc57de437f77a610dd82ffdfb42
gt-pg1 <shot66766@gmail.com>
2023-06-29 16:20:16 +0400

    Deleted unusable imports from views.py

--- SoulMatcher/soulmate/views.py
+++ SoulMatcher/soulmate/views.py
@@ -2,14 +2,14 @@ import uuid
 
 from django.contrib.auth import get_user_model
 
-from rest_framework import status, viewsets, views
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from rest_framework_simplejwt.views import TokenObtainPairView as SimpleTokenObtainPairView
 
-from .models import Priority, CustomUser
+from .models import Priority
 from .serializers import \
     UserSerializer, \
     CustomTokenObtainPairSerializer, \
