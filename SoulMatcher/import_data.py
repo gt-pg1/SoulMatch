87418287d7a2dc1015a5eec401df20a2ed4cd92f
gt-pg1 <shot66766@gmail.com>
2023-06-29 16:18:41 +0400

    Updated import script

--- SoulMatcher/import_data.py
+++ SoulMatcher/import_data.py
@@ -1,36 +1,35 @@
+from tqdm import tqdm
 import json
+import re
 import uuid
 import transliterate
-import re
-from tqdm import tqdm
-
 from django.db import transaction
-
-from soulmate.models import CustomUser, Priority
+from soulmate.models import CustomUser, Aspect, Attitude, Weight, Priority
 
 
 def import_data_from_json(file_name):
     users_to_create = []
-    priorities_to_create = []
+    priority_relations = []
 
-    # Открываем файл и считаем количество строк
     with open(file_name, 'r', encoding='utf-8') as file:
         total_lines = sum(1 for _ in file)
 
-    # Открываем файл снова и создаем прогресс-бар для чтения файла
+    aspects_cache = {aspect.aspect: aspect for aspect in Aspect.objects.all()}
+    attitudes_cache = {attitude.attitude: attitude for attitude in Attitude.objects.all()}
+    weights_cache = {weight.weight: weight for weight in Weight.objects.all()}
+
     with open(file_name, 'r', encoding='utf-8') as file:
-        for index, line in enumerate(tqdm(file, total=total_lines, desc="Reading file")):
+        for index, line in enumerate(tqdm(file, total=total_lines, desc="Reading file", leave=False)):
             record = json.loads(line)
             name = record.get('name')
-            precedents = record.get('precedents', {})
+            aspects = record.get('precedents', {})
 
             surname, firstname = name.split(' ')
             latin_surname = re.sub(r'[^a-zA-Z0-9.@]', '', transliterate.translit(surname, reversed=True).lower())
             latin_firstname = re.sub(r'[^a-zA-Z0-9.@]', '', transliterate.translit(firstname, reversed=True).lower())
-            email = f"{latin_surname}.{latin_firstname}@test.com"
             username = f"{latin_surname}{latin_firstname}{100000 + index}"
+            email = f"{username}@test.com"
 
-            # Создание пользователя
             user = CustomUser(
                 username=username,
                 first_name=firstname,
@@ -42,32 +41,38 @@ def import_data_from_json(file_name):
             )
             users_to_create.append(user)
 
-    # Использование транзакции для ускорения вставки данных
+            for aspect_name, details in aspects.items():
+                attitude_name = details.get('attitude')
+                weight_value = details.get('importance')
+
+                aspect = aspects_cache.get(aspect_name)
+                if aspect is None:
+                    aspect = Aspect.objects.create(aspect=aspect_name)
+                    aspects_cache[aspect_name] = aspect
+
+                attitude = attitudes_cache.get(attitude_name)
+                if attitude is None:
+                    attitude = Attitude.objects.create(attitude=attitude_name)
+                    attitudes_cache[attitude_name] = attitude
+
+                weight = weights_cache.get(weight_value)
+                if weight is None:
+                    weight = Weight.objects.create(weight=weight_value)
+                    weights_cache[weight_value] = weight
+
+                priority_relations.append((user, aspect, attitude, weight))
+
     with transaction.atomic():
-        # Добавляем пользователей в БД и отображаем прогресс
-        user_progress_bar = tqdm(total=len(users_to_create), desc="Writing users to DB")
-        created_users = CustomUser.objects.bulk_create(users_to_create)
-        user_progress_bar.update(len(users_to_create))
-        user_progress_bar.close()
+        tqdm.write("Writing users to DB...")
+        CustomUser.objects.bulk_create(users_to_create)
 
-        # Добавляем приоритеты в БД и отображаем прогресс
-        for user in created_users:
-            for aspect, details in precedents.items():
-                attitude = details.get('attitude')
-                weight = details.get('importance')
-
-                priority = Priority(
-                    user=user,
-                    aspect=aspect,
-                    attitude=attitude,
-                    weight=weight
-                )
-                priorities_to_create.append(priority)
-
-        priority_progress_bar = tqdm(total=len(priorities_to_create), desc="Writing priorities to DB")
-        Priority.objects.bulk_create(priorities_to_create)
-        priority_progress_bar.update(len(priorities_to_create))
-        priority_progress_bar.close()
+        tqdm.write("Writing priorities to DB...")
+        progress_bar = tqdm(total=len(priority_relations), desc="Writing priorities to DB", leave=False)
+        for user, aspect, attitude, weight in priority_relations:
+            priority = Priority.objects.create(aspect=aspect, attitude=attitude, weight=weight)
+            priority.users.add(user)
+            progress_bar.update(1)
+        progress_bar.close()
 
 
 import_data_from_json("participants.jsonl")
